b5ea050168be3b82b0f478f6ddd18815d4929818
Joey Liu <qizhi.liu@joinhandshake.com>
2026-03-26 11:20:10 -0700

    chore: add TODO comments for domain change to svcollegechurch.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  // TODO: update to https://svcollegechurch.org when domain is live
   site: 'https://sv-college-church.github.io',
   integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
   output: 'static',
